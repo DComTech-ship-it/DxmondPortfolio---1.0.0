29e4d14ae6a9bd0dbc3efc7fc8099dbc4c47baba
Your GitHub Username <dxmondtecku@gmail.com>
2026-01-14 19:24:42 +0000

    Update index.tsx

--- src/app/components/home/typing-blog/index.tsx
+++ src/app/components/home/typing-blog/index.tsx
@@ -58,10 +58,10 @@ const TypingBlog = () => {
         <div className="border-x border-primary/10">
           <div className="flex flex-col max-w-3xl mx-auto py-10 px-4 sm:px-7">
             <div className="flex flex-col xs:flex-row gap-5 items-center justify-between">
-              <p className="text-sm tracking-[2px] text-primary uppercase font-medium">Latest Articles</p>
+              <p className="text-sm tracking-[2px] text-primary uppercase font-medium">My Highlights</p>
               <Button asChild variant={"outline"} className="h-auto">
                 <Link href={"/blog"} className="py-3 px-5">
-                  View All Articles
+                  View All Highlights
                 </Link>
               </Button>
             </div>
