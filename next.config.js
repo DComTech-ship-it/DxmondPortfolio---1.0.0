688978c27fed120e94331dd6219148cb634132a5
Your GitHub Username <dxmondtecku@gmail.com>
2026-01-15 13:13:29 +0000

    d

--- next.config.js
+++ next.config.js
@@ -1,15 +1,7 @@
-cat > next.config.js << 'EOL'
-/** @type {import('next').NextConfig} */
+﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
-  // Disable turbopack for now as it's causing issues
-  experimental: {
-    turbo: false
-  }
+  swcMinify: true
 }
 
 module.exports = nextConfig
-EOL
