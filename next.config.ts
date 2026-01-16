297932c43cdba9c8c5b994f19635708d7669507d
Your GitHub Username <dxmondtecku@gmail.com>
2026-01-16 01:14:59 +0000

    j

---
+++ next.config.ts
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  // Add your Next.js configuration here
+}
+
+module.exports = nextConfig
